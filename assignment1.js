da204ba53b3df1c86f88846e923f089a3d58000e
rracapili <rracapili@gmail.com>
2024-01-20 21:34:52 -0500

    first commit

---
+++ assignment1.js
@@ -0,0 +1,19 @@
+/*********************************************************************************
+*  WEB700 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Ronald Capili Student ID: radcapili Date: January 20, 2023
+*
+********************************************************************************/ 
+
+
+var serverVerbs = ["GET","GET","GET","POST","GET","POST"]
+var serverPaths = ["/","/about","/contact","/login","/panel","/logout"]
+var serverResponses = ["Welcome to WEB700 Assignment 1","This assignment was prepared by Student Name","Student Name: Student Email","User Logged In","Main Panel","Logout Complete"]
+
+// httpRequest function accepts two parameters, `httpVerb` and `path`.
+function httpRequest(httpVerb, path) {
+    return a + b;
+}
