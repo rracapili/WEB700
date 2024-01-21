e8cd222b8477985ecdd6750cc9a52fdd661d2072
rracapili <rracapili@gmail.com>
2024-01-20 22:44:10 -0500

    Time for manual testing

--- assignment1.js
+++ assignment1.js
@@ -11,9 +11,28 @@
 
 var serverVerbs = ["GET","GET","GET","POST","GET","POST"]
 var serverPaths = ["/","/about","/contact","/login","/panel","/logout"]
-var serverResponses = ["Welcome to WEB700 Assignment 1","This assignment was prepared by Student Name","Student Name: Student Email","User Logged In","Main Panel","Logout Complete"]
+var serverResponses = ["Welcome to WEB700 Assignment 1","This assignment was prepared by Ronald Capili","Ronald Capili: radcapili@myseneca.ca","User Logged In","Main Panel","Logout Complete"]
 
 // httpRequest function accepts two parameters, `httpVerb` and `path`.
 function httpRequest(httpVerb, path) {
-    return a + b;
+    var httpStatus = "404: "
+    var httpResponse = httpStatus + "Unable to process " + httpVerb + " request for " + path
+    for (var x = 0; x < serverPaths.length; x++) {
+        if (httpVerb == "GET"){
+            if (path == serverPaths[x] && (x==0 || x==1 || x==2 || x==4)){
+                httpStatus = "200: "
+                httpResponse=httpStatus+serverResponses[x]
+                break
+            }
+        }
+        else if (httpVerb == "POST"){
+            if (path == serverPaths[x] && (x==3 || x==5)){
+                httpStatus = "200: "
+                httpResponse=httpStatus+serverResponses[x]
+                break
+            }
+        }
+      }
+    console.log(httpResponse)
 }
+httpRequest("GET","/")
